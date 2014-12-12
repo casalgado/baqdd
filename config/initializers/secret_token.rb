ea326ee0456c19cff9ba18e7110dbda8cecc0df0
Carlos Salgado <casalgado86@yahoo.com>
2014-12-12 12:05:35 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Baqdd::Application.config.secret_key_base = '0074eafb844b9f0cff0cd36f80f2855f44ea92b230cd7ecf9a14d103f87a210e7e7c50caa892b226cffde66df8b73f9eb6b48819e9b52b2b7d03f184c1baa76c'
